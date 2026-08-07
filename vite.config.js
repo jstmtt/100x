ecf2779b12a858093b912625896e68968fa7b9cd
jstmtt <soyeahguys@gmail.com>
2026-08-07 23:17:03 +0200

    Brutalist portfolio tracker with GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/100x/',
     plugins: [
         react(),
         Icons({ compiler: 'jsx', jsx: 'react' }),
